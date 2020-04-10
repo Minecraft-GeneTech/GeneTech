ab02722cafbf2f6aeff53c5da1fcd14dcd8e3547
UtopiaXC <dys1025@sina.com>
2020-04-10 20:26:40 +0800

    针管

--- src/main/java/com/minecraft/genetech/common/CommonProxy.java
+++ src/main/java/com/minecraft/genetech/common/CommonProxy.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+@SuppressWarnings("InstantiationOfUtilityClass")
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         new itemLoader(event);
--- src/main/java/com/minecraft/genetech/items/Syringe.java
+++ src/main/java/com/minecraft/genetech/items/Syringe.java
@@ -1,17 +1,46 @@
 package com.minecraft.genetech.items;
 
-import javafx.scene.paint.Material;
-import net.minecraft.block.Block;
+import ibxm.Player;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemTool;
+import net.minecraft.item.ItemBed;
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.util.Set;
 
 public class Syringe extends Item {
 
-    public Syringe(){
+    public Syringe() {
         super();
         this.setUnlocalizedName("itemSyringe");
+        setContainerItem(this);
         maxStackSize = 16;
+    }
+
+
+
+    @SuppressWarnings("NullableProblems")
+    @SideOnly(Side.CLIENT)
+    public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
+        if (playerIn.getHeldItemMainhand().getItem() instanceof Syringe) {
+            if(!playerIn.addItemStackToInventory(new ItemStack(new Syringe().setUnlocalizedName(target.getName())))){
+                stack.shrink(1);
+                playerIn.addItemStackToInventory(new ItemStack(new Syringe().setUnlocalizedName(target.getName())));
+            }else
+                stack.shrink(1);
+            return true;
+        }
+        return false;
     }
 }
