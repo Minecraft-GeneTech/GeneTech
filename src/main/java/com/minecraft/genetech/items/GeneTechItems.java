50fda535dfa556b27738c35ae62e947b3594d6c9
UtopiaXC <dys1025@sina.com>
2020-04-11 13:39:17 +0800

    物品逻辑优化

--- src/main/java/com/minecraft/genetech/items/GeneTechItems.java
+++ src/main/java/com/minecraft/genetech/items/GeneTechItems.java
@@ -1,16 +1,6 @@
 package com.minecraft.genetech.items;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-
-import java.util.Objects;
-
 public class GeneTechItems extends Item {
     public static String Unlocal;
     public static Item item;
