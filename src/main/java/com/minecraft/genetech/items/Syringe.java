f2176a768695d1d11bbfc08d51686fdef8d50232
UtopiaXC <dys1025@sina.com>
2020-04-11 13:38:45 +0800

    物品逻辑优化

--- src/main/java/com/minecraft/genetech/items/Syringe.java
+++ src/main/java/com/minecraft/genetech/items/Syringe.java
@@ -1,8 +1,11 @@
 package com.minecraft.genetech.items;
 
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -10,17 +13,18 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class Syringe extends GeneTechItems {
 
     public Syringe() {
-        super("itemSyringe",16,"genetech:syringe");
+        super("itemSyringe", 16, "genetech:syringe");
     }
 
     @SuppressWarnings("NullableProblems")
     @SideOnly(Side.CLIENT)
     public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
         if (playerIn.getHeldItemMainhand().getItem() instanceof Syringe) {
-            if(!playerIn.addItemStackToInventory(new ItemStack(itemLoader.items.get("genetech:syringe_full")))){
+            ItemStack itemStack = new ItemStack(itemLoader.items.get("genetech:syringe_full"));
+            if (!playerIn.addItemStackToInventory(itemStack)) {
                 stack.shrink(1);
-                playerIn.addItemStackToInventory(new ItemStack(itemLoader.items.get("genetech:syringe_full")));
-            }else
+                playerIn.addItemStackToInventory(itemStack);
+            } else
                 stack.shrink(1);
             return true;
         }
